95f0f851424824e8ca3b599dd593f2519a93d882
Clapinton <eric.olivetree@gmail.com>
2017-03-30 16:07:59 -0700

    Add config for npm packages and webpack bundler

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  //Entry and output points
+  entry: './frontend/expense_trackster.jsx',
+  output: {
+    path: 'app/assets/javascripts', //Use this path on rails apps
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/], //for files ending in this...
+        exclude: /(node_modules)/, //exclude node_modules
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map', //Get line references when errors are raised
+  resolve: {
+    extensions: ['', '.js', '.jsx' ] //so you can type require "/lib/MyClass" and ommit the .js
+  }
+};
