b00301f4cb2f34d9a5293fedafbcedaf26837ebf
Clapinton <eric.olivetree@gmail.com>
2017-03-30 13:59:43 -0700

    Add models for users and expenses

---
+++ db/migrate/20170330204230_create_expenses.rb
@@ -0,0 +1,13 @@
+class CreateExpenses < ActiveRecord::Migration
+  def change
+    create_table :expenses do |t|
+      t.integer :owner_id, null: false
+      t.datetime :datetime, null: false
+      t.float :amount, null: false
+      t.string :description, null: false
+
+      t.timestamps null: false
+    end
+    add_index :expenses, :owner_id
+  end
+end
