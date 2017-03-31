9d96e5944ecb789c7ee602a6b6046c76b5a0a6ac
Clapinton <eric.olivetree@gmail.com>
2017-03-31 09:52:40 -0700

    Auto generated test files

--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  owner_id    :integer          not null
+#  datetime    :datetime         not null
+#  amount      :float            not null
+#  description :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
