141b4cc373fcd4a1ca5ff1ec53a523074e122735
Clapinton <eric.olivetree@gmail.com>
2017-03-31 16:59:45 -0700

    Add specs to verify expense amount digit

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -13,6 +13,16 @@ RSpec.describe Expense, :type => :model do
       expect(expense).not_to be_valid
     end
 
+    it "validates amount digits" do
+      expense_many_digits = Expense.new(amount: '123.456', owner_id: 1, datetime: '1414-12-14T14:12:00.000Z', description: 'Test expense')
+      expect(expense_many_digits).not_to be_valid
+      expense_few_digits = Expense.new(amount: '123.4', owner_id: 1, datetime: '1414-12-14T14:12:00.000Z', description: 'Test expense')
+      expect(expense_few_digits).not_to be_valid
+      expense_no_digits = Expense.new(amount: '123', owner_id: 1, datetime: '1414-12-14T14:12:00.000Z', description: 'Test expense')
+      expect(expense_no_digits).to be_valid
+    end
+
+
     it "validates owner presence" do
       expense = Expense.new(amount: "123.45", datetime: '1414-12-14T14:12:00.000Z', description: 'Test expense')
       expect(expense).not_to be_valid
@@ -32,7 +42,6 @@ RSpec.describe Expense, :type => :model do
       expense = Expense.new(amount: "123.45", owner_id: 1, datetime: '1414-12-14T14:12:00.000Z', description: 'Text expense')
       expect(expense).to be_valid
     end
-
 
   end
 end
