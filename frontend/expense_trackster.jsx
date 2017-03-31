bee5bf30520a0563e5e27d42ca178deac076d54d
Clapinton <eric.olivetree@gmail.com>
2017-03-30 17:06:44 -0700

    Drop Redux approach in lieu of pure React

--- frontend/expense_trackster.jsx
+++ frontend/expense_trackster.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
-import configureStore from './store/store';
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById("root");
-  let store;
+  let preloadedState = {};
 
   //We need this to keep the user logged in, even if they refresh the page (user bootstraping)
   if (window.currentUser) {
-    const preloadedState = {
+    preloadedState = {
       session: {
         currentUser: window.currentUser,
         errors: []
       }
     }
-    store = configureStore(preloadedState);
-  } else {
-    store = configureStore();
   };
 
-  ReactDOM.render(<Root store={store} />, rootEl);
+  ReactDOM.render(<Root store={preloadedState} />, rootEl);
 
 
 });
