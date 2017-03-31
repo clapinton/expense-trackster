5e351cf6cc0e1a50c534907aeac94572336ddc9c
Clapinton <eric.olivetree@gmail.com>
2017-03-31 11:48:10 -0700

    Require report owner_id

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,7 +3,7 @@
 # Table name: reports
 #
 #  id         :integer          not null, primary key
-#  created_by :integer
+#  created_by :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
