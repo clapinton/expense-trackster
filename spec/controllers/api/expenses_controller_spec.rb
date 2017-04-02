df9dc75ec2c5d31caff3210222b3fc2350ae0612
Clapinton <eric.olivetree@gmail.com>
2017-04-01 21:21:01 -0700

    Add spec for #patch method

--- spec/controllers/api/expenses_controller_spec.rb
+++ spec/controllers/api/expenses_controller_spec.rb
@@ -202,8 +202,8 @@ RSpec.describe Api::ExpensesController, type: :controller do
     context "when not logged in" do
 
       it "returns 403" do
-        new_expense = {amount: "543.21", owner_id: admin.id, datetime: '1414-12-14T14:12:00.000Z', description: 'POSTed by an admin'}
-        post :create, {expense: new_expense, format: :json}
+        updated_expense = {amount: "543.21", owner_id: admin.id, datetime: '1414-12-14T14:12:00.000Z', description: 'POSTed by an admin'}
+        patch :update, {id: expense_admin.id, expense: updated_expense, format: :json}
         expect(response).to have_http_status(403)
       end
     end
@@ -212,6 +212,55 @@ RSpec.describe Api::ExpensesController, type: :controller do
 
       before do
         allow(controller).to receive(:current_user) { admin }
+      end
+
+      it "should allow update to own expense" do
+        updated_expense = {amount: "543.21", owner_id: admin.id, datetime: '1414-12-14T14:12:00.000Z', description: 'POSTed by an admin'}
+        patch :update, {id: expense_admin.id, expense: updated_expense, format: :json}
+        expect(response).to have_http_status(200)
+        get :show, id: expense_admin.id, format: :json
+        json_response = JSON.parse(response.body)
+        expect(json_response["amount"]).to eq("543.21")
+      end
+
+      it "should not allow update to another user's expense" do
+        updated_expense = {amount: "543.21", owner_id: admin.id, datetime: '1414-12-14T14:12:00.000Z', description: 'POSTed by an admin'}
+        patch :update, {id: expense_user.id, expense: updated_expense, format: :json}
+        expect(response).to have_http_status(403)
+      end
+
+      it "should not update with an invalid param" do
+        updated_expense = {amount: "543.222", owner_id: admin.id, datetime: '1414-12-14T14:12:00.000Z', description: 'POSTed by an admin'}
+        patch :update, {id: expense_admin.id, expense: updated_expense, format: :json}
+        expect(response).to have_http_status(422)
+      end
+    end
+
+    context "when logged in as a user" do
+
+      before do
+        allow(controller).to receive(:current_user) { user }
+      end
+
+      it "should allow update to own expense" do
+        updated_expense = {amount: "98.76", owner_id: user.id, datetime: '1414-12-14T14:12:00.000Z', description: 'POSTed by an user'}
+        patch :update, {id: expense_user.id, expense: updated_expense, format: :json}
+        expect(response).to have_http_status(200)
+        get :show, id: expense_user.id, format: :json
+        json_response = JSON.parse(response.body)
+        expect(json_response["amount"]).to eq("98.76")
+      end
+
+      it "should not allow update to another user's expense" do
+        updated_expense = {amount: "98.76", owner_id: user.id, datetime: '1414-12-14T14:12:00.000Z', description: 'POSTed by an user'}
+        patch :update, {id: expense_admin.id, expense: updated_expense, format: :json}
+        expect(response).to have_http_status(403)
+      end
+
+      it "should not update with an invalid param" do
+        updated_expense = {amount: "98.766", owner_id: user.id, datetime: '1414-12-14T14:12:00.000Z', description: 'POSTed by an user'}
+        patch :update, {id: expense_user.id, expense: updated_expense, format: :json}
+        expect(response).to have_http_status(422)
       end
     end
 
