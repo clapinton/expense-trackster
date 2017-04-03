12dd56af76c5e74dc93de6d2aa8fecb95dae54ea
Clapinton <eric.olivetree@gmail.com>
2017-04-02 19:52:35 -0700

    Change file structure so each component has its own folder

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { logout } from '../../api/session_api';
 import { getAllExpenses } from '../../api/expenses_api';
-import ExpenseForm from '../expenses/expense_form';
-import ExpenseList from '../expenses/expense_list';
+import ExpenseForm from '../expense_form/expense_form';
+import ExpenseList from '../expense_list/expense_list';
+import ExpenseReport from '../expense_report/expense_report';
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -11,6 +12,9 @@ class Dashboard extends React.Component {
     this.handleLogout = this.handleLogout.bind(this);
     this.logoutSuccess = this.logoutSuccess.bind(this);
 
+    // Since we won't be using redux, the state of the dashboard will act as the main store.
+    // When a expenses is created/edited, the AJAX returns an updated list of expenses.
+    // This list is passed to the ExpenseList component.
     this.state = {
       expense: {},
       expenseList: []
@@ -56,6 +60,7 @@ class Dashboard extends React.Component {
         <h1>Dashboard</h1>
         <ExpenseForm expense={this.state.expense} saveSuccess={this.saveSuccess}/>
         <ExpenseList expenseList={this.state.expenseList} selectExpenseToEdit={this.selectExpenseToEdit}/>
+        <ExpenseReport/>
       <button onClick={this.handleLogout}>Logout</button>
       </div>
     )
